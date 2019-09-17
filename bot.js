1dd4b56491a33605387e920fa8a12dfc143d9e6a
THEBESTspamer <55215527+THEBESTspamer@users.noreply.github.com>
2019-09-17 22:00:21 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("623558204529311755")
+setInterval(function() {
+channel.send(`dsjkfihfiopSFKASRJGPLARKLGOPGUOEAJODSJGOREJFPOSDFW894937R8346?>"l:;';l[pksdfpa[giooewafoidmakkkkkkkajfkarhgkler238+.-*/*623dsfsddsfds)(*&&^%%$$#@@!!!QWEL{:"""::?:L:||`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
